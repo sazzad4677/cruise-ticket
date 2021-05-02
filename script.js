1a66c35c13666222c2724031c1b93bda36ecbd4d
sazzad4677 <sazzad35-1898@diu.edu.bd>
2021-05-02 11:52:03 +0600

    calculation fixed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//ticket values add or subtract
 function handelTicketChange(ticketClass, isIncrease){
     const ticketInput= document.getElementById(ticketClass + '-class-input');
     const ticketCount = getInputValue(ticketClass);
@@ -11,7 +12,7 @@ function handelTicketChange(ticketClass, isIncrease){
     ticketInput.value = newTicketValue;
     totalCalculation();
 }
-
+// calculation of ticket,tax,grand total
 function totalCalculation(){
    const firstClassCount = getInputValue('first');
    const economyClassCount = getInputValue('economy');
@@ -25,16 +26,18 @@ function totalCalculation(){
    document.getElementById('grand-total').innerText = '$'+ grandtotal;
 }
 
+//get input values of ticket
 function getInputValue(ticketClass){
     const ticketInput = document.getElementById(ticketClass + '-class-input');
     const ticketCount = parseInt(ticketInput.value);
     return ticketCount;
 }
 
-
+//booking form hide after successful order completed 
 function bookingFormHide (){
     const firstClassCount = getInputValue('first');
     const economyClassCount = getInputValue('economy');
+    //if ticket selected then submit form will hide
     if (firstClassCount > 0 || economyClassCount> 0) {
         const  submitForm = document.getElementById("submit-form");
         submitForm.style.display = "none"; 
@@ -42,15 +45,17 @@ function bookingFormHide (){
         const successForm = document.getElementById("success-form");
         successForm.style.display = 'block';
     }
+    //if no ticket selected a popup window will appear
     if (firstClassCount == 0 && economyClassCount == 0) {
         alert("Please select at least 1 ticket");
     }
 }
 
+//for home button
 function bookingFormShow(){
     const  submitForm = document.getElementById("submit-form");
     submitForm.style.display = "block"; 
-    
+
     const successForm = document.getElementById("success-form");
     successForm.style.display = 'none';
 }
